d737122a29205b5f89ba4b900b9409c2a7973e37
Klombox <kennridgelomboy@gmail.com>
2023-03-12 20:30:45 -0400

    Updated PlaceObject and Packages

--- Assets/ProjectScripts/PlaceObject.cs
+++ Assets/ProjectScripts/PlaceObject.cs
@@ -9,18 +9,20 @@ using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch;
 public class PlaceObject : MonoBehaviour
 {
     [SerializeField]
-    private GameObject prefab;  //ref to prefab to spawn
+    private GameObject prefab; //ref to prefab to spawn
     [SerializeField]
     private int numToSpawn = 4; //number of game objects to spawn
 
-    private ARRaycastManager aRRaycastManager;  //ref to raycast manager
-    private ARPlaneManager aRPlaneManager;  //ref to plane manager
+    private ARRaycastManager aRRaycastManager; //ref to raycast manager
+    private ARPlaneManager aRPlaneManager; //ref to plane manager
     private List<ARRaycastHit> hits = new List<ARRaycastHit>(); //to hold hitResults
-    private int numSpawned = 0; //counter variable for the number of objects spawned
+    private int numSpawned = 0; //variable for the number of objects spawned
+    private Vector3 cameraPosition; //ref to position of phone camera
 
     private void Awake(){
         aRRaycastManager = GetComponent<ARRaycastManager>();
         aRPlaneManager = GetComponent<ARPlaneManager>();
+        cameraPosition = Camera.main.transform.position; //cached camera position
     }
 
     private void OnEnable(){
@@ -32,37 +34,32 @@ public class PlaceObject : MonoBehaviour
     private void OnDisable(){
         EnhancedTouch.TouchSimulation.Disable(); //disables touch simulation in editor (for testing)
         EnhancedTouch.EnhancedTouchSupport.Disable(); //disables EnhancedTouch
-        EnhancedTouch.Touch.onFingerDown -= FingerDown;
+        EnhancedTouch.Touch.onFingerDown -= FingerDown; //unsubs to event
     }
 
-    private void FingerDown(EnhancedTouch.Finger finger){
+    //function to check the fingers touching phone screen (only one finger)
+    private void FingerDown(EnhancedTouch.Finger finger)
+    {
         if (finger.index != 0) return;
+        SpawnObjects(finger.currentTouch.screenPosition); //ref to spawning function
+    }
 
-        //cast raycast
-        if(aRRaycastManager.Raycast(finger.currentTouch.screenPosition, hits, TrackableType.PlaneWithinPolygon) ){
-
+    //function to spawn objects
+    private void SpawnObjects(Vector2 touchPosition)
+    {
+        if(aRRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
             //check if the desired number of objects is already spawned
             if (numSpawned >= numToSpawn) {
                 return;
             }
 
-            foreach (ARRaycastHit hit in hits)
-            {
+            //foreach loop to iterate through hits
+            foreach (ARRaycastHit hit in hits) {
                 Pose pose = hit.pose;
                 GameObject obj = Instantiate(prefab, pose.position, pose.rotation);
 
-                //check for certain AR plane (Horizontal,Vertical, etc.)
-                if(aRPlaneManager.GetPlane(hit.trackableId).alignment == PlaneAlignment.Vertical) //example was PlaneAlignment.HorizontalUp
-                {
-                    //rotate obj in direction of position
-                    Vector3 position = obj.transform.position;
-                    position.y = 0f;
-                    Vector3 cameraPosition = Camera.main.transform.position;
-                    cameraPosition.y = 0f;
-                    Vector3 direction = cameraPosition - position;
-                    Quaternion targetRotation = Quaternion.LookRotation(direction);
-                    obj.transform.rotation = targetRotation;
-                }
+                RotateObjectOnPlane(obj, hit.trackableId); //ref to rotating function
 
                 numSpawned++; //increment counter variable
 
@@ -70,8 +67,21 @@ public class PlaceObject : MonoBehaviour
                 if (numSpawned >= numToSpawn) {
                     break;
                 }
-
             }
+        }
+    }
+
+    //function for rotating object on plane
+    private void RotateObjectOnPlane(GameObject obj, TrackableId trackableId)
+    {
+        ARPlane plane = aRPlaneManager.GetPlane(trackableId);
+        if (plane.alignment == PlaneAlignment.Vertical)
+        {
+            Vector3 position = obj.transform.position;
+            position.y = 0f;
+            Vector3 direction = cameraPosition - position;
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            obj.transform.rotation = targetRotation;
         }
     }
 }
